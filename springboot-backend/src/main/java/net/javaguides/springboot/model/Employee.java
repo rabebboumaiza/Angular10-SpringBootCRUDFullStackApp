1aae5ca0ab4544ee091784331621b31506a7ce7f
rabebboumaiza <rabebboumaiza18@gmail.com>
2024-04-22 03:17:27 +0200

    firstcommit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,67 @@
+package net.javaguides.springboot.model;
+
+
+
+
+import jakarta.annotation.Generated;
+//import javax.persistence.Entity;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name= "employee")
+
+public class Employee {
+	@Id
+	@GeneratedValue( strategy= GenerationType.IDENTITY)
+	
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email_id")
+	private String EmailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		EmailId = emailId;
+	}
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return EmailId;
+	}
+	public void setEmailId(String emailId) {
+		EmailId = emailId;
+	}
+	
+	
+
+}
